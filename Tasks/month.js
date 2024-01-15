bf803be8cfe7519127fd7795e9d38997af2656b5
Distantlandof <156641491+Distantlandof@users.noreply.github.com>
2024-01-15 13:34:42 +0200

    Update month2.js

--- Tasks/month.js
+++ Tasks/month.js
@@ -5,8 +5,8 @@ const getMonth = (input) => {
     'jan', 'feb', 'mar', 'apr', 'may', 'jun',
     'jul', 'aug', 'sep', 'oct', 'nov', 'dec'
   ];
-  for (let i = 0; i < l; i++) {
-    if (s.toLowerCase().startsWith(getMonth[i])) return i + 1;
+const lowerInput = input.toLowerCase();
+  const index = months.findIndex(month => lowerInput.startsWith(month));
   }
  return index !== -1 ? index + 1 : -1;
 };
