49aa1bbc5e57c269320259ca7ee6a7cc7888ee0a
Distantlandof <156641491+Distantlandof@users.noreply.github.com>
2024-01-15 11:47:03 +0200

    Update between.js

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,19 +3,16 @@
 getvaluebetween = (str, p, s) => {
   i = str.indexOf(p);
   if (i === -1) return '';
-  else {
     k = i + p.length;
     str = str.substring(k);
     if (s) {
       i = str.indexOf(s);
       if (i === -1) {
         return '';
-      } else {
         str = str.substring(0, i);
-      }
     }
   }
   return str;
-};
+}; 
 
 module.exports = getvaluebetween;
