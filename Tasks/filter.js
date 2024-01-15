3be3b7df973dfb1a7c4aeeefef4f2c865293d612
Distantlandof <156641491+Distantlandof@users.noreply.github.com>
2024-01-15 13:22:28 +0200

    Update filter4.js

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -2,12 +2,14 @@
 
 'use strict'; 
 
+require('@ungap/structured-clone');
+
 const Filter = (array, type) => {
   const res = [];
   
  for (const value of Object.values(array)) {
     if (typeof value === type) {
-      res.push(value);
+      res.push(structuredClone(value));
     }
   }
   return res;
